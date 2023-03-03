3173b11a013eb39b76ffad4163d90d4216206c7a
Leo Coria <leo.coria92@gmail.com>
2023-03-02 21:56:14 -0300

    Aplico BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar />
-    <ItemListContainer greeting="Welcome to Stoica Shoes Store!!!"/>
+    <Routes>
+      <Route exact path="/" element={<ItemListContainer greeting="Welcome to Stoica Shoes Store!!!" />}/>
+      <Route exact path="/category/:id" element={<ItemListContainer greeting="Envios gratis en compras mayores a $10000" />}/>
+      <Route exact path="/item/:id" element={<ItemDetailContainer />}/>
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,49 @@
-import React from 'react'
-import logoStoica from '../../img/logo/logoStoica.png'
-import CartWidget from '../CartWidget/CartWidget'
-import './Navbar.css'
+import React from "react";
+import logoStoica from "../../img/logo/logoStoica.png";
+import CartWidget from "../CartWidget/CartWidget";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src={logoStoica} className="logo" alt="Logo Stoica" /></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Men</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Women</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Kids</a>
-        </li>
-      </ul>
-      <CartWidget />
-    </div>
-  </div>
-</nav>
-</header>
-  )
-}
+      <nav
+        class="navbar navbar-expand-lg bg-body-tertiary bg-dark"
+        data-bs-theme="dark"
+      >
+        <div class="container-fluid">
+          <Link to={"/"}>
+            <img src={logoStoica} className="logo" alt="Logo Stoica" />
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link to={"/category/men"}>Men</Link>
+              </li>
+              <li class="nav-item">
+                <Link to={"/category/women"}>Women</Link>
+              </li>
+            </ul>
+            <CartWidget />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
